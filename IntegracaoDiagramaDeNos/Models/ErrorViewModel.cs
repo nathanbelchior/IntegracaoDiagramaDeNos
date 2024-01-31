efac4adc8b8372f92a75a0038b2e540dca14248b
NATHAN SILVA <nathan.silva@mpm.mp.br>
2024-01-31 14:58:07 -0300

    Adicionar arquivos de projeto.

---
+++ IntegracaoDiagramaDeNos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IntegracaoDiagramaDeNos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
