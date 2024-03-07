b726ff66443d5c435b9fc1a84ef3d93af1794c22
NATHAN SILVA <nathan.silva@mpm.mp.br>
2024-03-07 18:00:43 -0300

    Incremento com a exportação do diagrama no formato de imagem

--- IntegracaoDiagramaDeNos/ViewModel/HomeViewModel.cs
+++ IntegracaoDiagramaDeNos/ViewModel/HomeViewModel.cs
@@ -11,6 +11,8 @@ namespace IntegracaoDiagramaDeNos.ViewModel
             public int Id { get; set; }
             public string? label { get; set; }
             public string? color { get; set; }
+            public int? origem { get; set; }
+            public int? destino { get; set; }
         }
         public HomeViewModel()
         {
@@ -30,22 +32,32 @@ namespace IntegracaoDiagramaDeNos.ViewModel
                     case 1:
                         investigado.label = "Fabricio";
                         investigado.color = "SaddleBrown";
+                        investigado.origem = item;
+                        investigado.destino = 2;
                         break;
                     case 2:
                         investigado.label = "Felipe";
                         investigado.color = "DarkGreen";
+                        investigado.origem = item;
+                        investigado.destino = 3;
                         break;
                     case 3:
-                        investigado.label = "Guilherme";
+                        investigado.label = "Guilherme Henrique";
                         investigado.color = "DarkCyan";
+                        investigado.origem = item;
+                        investigado.destino = 4;
                         break;
                     case 4:
                         investigado.label = "Nathan";
                         investigado.color = "MidnightBlue";
+                        investigado.origem = item;
+                        investigado.destino = 5;
                         break;
                     case 5:
                         investigado.label = "Wallace";
                         investigado.color = "SlateBlue";
+                        investigado.origem = item;
+                        investigado.destino = 1;
                         break;
 
                     default:
